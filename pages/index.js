02186da3e2d63bf5af37f0bfeed8d87d43800d72
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-08-10 19:43:41 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Car Category | MVRNFT</title>
+        <title>Tesla Collection | MVRNFT</title>
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
