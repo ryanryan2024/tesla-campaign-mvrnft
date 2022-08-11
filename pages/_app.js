efe3dfbc64fcb4232ef3254fad4f4c155ca6eec5
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-08-10 17:17:38 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 
+const supportedChainIds = [1, 4, 137];
+
+const connectors = {
+  injected: {}
+};
+
 const MyApp = ({ Component, pageProps }) => {
     return (
         <ThirdwebProvider desiredChainId={ChainId.Goerli} chainRpc={{[ChainId.Goerli]: 'https://goerli.infura.io/v3/9baab33bcd46485c96f25fd1f34c4f38'}}>
